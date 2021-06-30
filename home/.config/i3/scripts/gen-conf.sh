6045ad9d966be83456b40f43430b47d84d42640e
laur <laur.aliste@gmail.com>
2021-06-30 04:44:22 +0200

    i3/gen-conf.sh: create [$hostname] variable in i3 config

--- home/.config/i3/scripts/gen-conf.sh
+++ home/.config/i3/scripts/gen-conf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # base from http://mrtango.planetcrazy.de/dynamic-screen-definitions-in-i3wm.html
+#
+# Note this script's stdout will be set as i3 config file!
 #####################################
 
 source /etc/.global-bash-init
@@ -8,7 +10,9 @@ _init || exit 1
 
 
 #### Entry ####
-
+echo '# vim:filetype=i3'
+echo '#'
+echo "set \$hostname $HOSTNAME"
 xrandr --listactivemonitors | awk '$1 ~ /^0/ {print "set $mainscreen " $4}'
 xrandr --listactivemonitors | awk '$1 ~ /^1/ {print "set $sidescreen " $4}'
 
--- home/.config/i3/scripts/refresh-settings.sh
+++ home/.config/i3/scripts/refresh-settings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # base from http://mrtango.planetcrazy.de/dynamic-screen-definitions-in-i3wm.html
 
-SCRIPT_DIR="$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
+SCRIPT_DIR="$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"  # note realpath is important here!
 DIR_UP="$(dirname -- "$SCRIPT_DIR")"
 #####################################
 
@@ -9,8 +9,9 @@ source /etc/.global-bash-init
 _init || exit 1
 ###############
 
-report "dir is [$SCRIPT_DIR]"
+
 #### Entry ####
+report "execution dir is [$SCRIPT_DIR]"
 "$SCRIPT_DIR"/gen-conf.sh > "$DIR_UP"/config.d/10-generated || exit 1
 update-conf.py -f "$DIR_UP"/config || exit 1
 
