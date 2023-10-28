6c1daa9167bd800b76884a1302e843c50cda2b60
laur <laur.aliste@gmail.com>
2023-10-28 13:46:08 +0200

    .mutt/detectMailboxes.sh: shorten shell boilerplate

--- .mutt/detectMailboxes.sh
+++ .mutt/detectMailboxes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This script decides which mailboxes to monitor, based on system we're currently on.
 #
@@ -6,27 +6,8 @@
 # hostnames are configured in .bash_env_vars
 #
 ########################################################################
-# env vars import has to be the first thing:
-if [[ "$__ENV_VARS_LOADED_MARKER_VAR" != "loaded" ]]; then
-    USER_ENVS=/etc/.bash_env_vars
-
-    if [[ -r "$USER_ENVS" ]]; then
-        source "$USER_ENVS"
-    else
-        echo -e "\n    ERROR: env vars file [$USER_ENVS] not found! Abort."
-        #exit 1  # TODO: exit?
-    fi
-fi
-
-# import common:
-if ! type __COMMONS_LOADED_MARKER > /dev/null 2>&1; then
-    if [[ -r "$_SCRIPTS_COMMONS" ]]; then
-        source "$_SCRIPTS_COMMONS"
-    else
-        echo -e "\n    ERROR: common file [$_SCRIPTS_COMMONS] not found or isn't readable! Abort."
-        exit 1
-    fi
-fi
+source /etc/.global-bash-init
+_init || exit 1
 
 ########################################################################
 readonly PERSONAL_BOX=~/.mutt/accounts/mailboxes.personal
