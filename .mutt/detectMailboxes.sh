ee343da109d20af922aca28cea7272b9e15f94d2
laur <laur.aliste@gmail.com>
2025-10-28 15:26:17 +0100

    mutt/detectMailboxes: update config syntax and check for work box existence

--- .mutt/detectMailboxes.sh
+++ .mutt/detectMailboxes.sh
@@ -5,12 +5,17 @@
 # sourced by muttrc.
 # hostnames are configured in .bash_env_vars
 #
+# - see also 'named-mailboxes' that takes pairs of params and allows to choose the
+#   display name for each:   named-mailboxes "Work (boo!)" =work
+#
 ########################################################################
 source /etc/.global-bash-init || exit 1
 
 ########################################################################
-readonly PERSONAL_BOX=~/.mutt/accounts/mailboxes.personal
-readonly WORK_BOX=~/.mutt/accounts/mailboxes.work
+readonly PERSONAL_BOX="$HOME/.mutt/accounts/mailboxes.personal"
+readonly WORK_BOX="$HOME/.mutt/accounts/mailboxes.work"
+# note as per https://github.com/neomutt/neomutt/issues/1901#issuecomment-547676531
+# the fake separator-dirs need to exist:
 readonly WORK_BOX_SEPARATOR="\nmailboxes +work/--------------"
 ########################################################################
 
@@ -18,15 +23,17 @@ if is_work; then
     commands="$(cat -- "$WORK_BOX")"
     #commands+="$PERSONAL_BOX_SEPARATOR"
     #commands="$(cat -- "$PERSONAL_BOX")"
-    commands+="\nset spoolfile = +work/Inbox"
+    commands+="\nset spool_file = +work/Inbox"
 #elif ! is_laptop; then
     #commands="$(cat -- "$PERSONAL_BOX")"
 else
     # select all
     commands="$(cat -- "$PERSONAL_BOX")"
-    commands+="$WORK_BOX_SEPARATOR"
-    commands+="$(cat -- "$WORK_BOX")"
-    commands+="\nset spoolfile = +personal/INBOX"
+    if [[ -d "$HOME/mail/work" && -s "$WORK_BOX" ]]; then
+        commands+="$WORK_BOX_SEPARATOR"
+        commands+="$(cat -- "$WORK_BOX")"
+    fi
+    commands+="\nset spool_file = +personal/INBOX"
 fi
 
 echo -e "$commands"
